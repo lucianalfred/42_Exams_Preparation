6fc42a6ad048acfacc5ba3c6e4325d92ffab145b
Lucian Alfred <lucianalfred60@gmailcom>
2025-12-23 21:25:39 +0100

    feat: scan int AND STRING

--- rank03/ft_scanf.c
+++ rank03/ft_scanf.c
@@ -55,3 +55,84 @@ int scan_char(FILE *f, va_list ap)
     return (1);
 
 }
+
+int scan_int(FILE *f, va_list ap)
+{
+    int c = fgetC(f);
+
+    if (c == EOF)
+    {
+        if (ferror(f))
+            return -1;
+        return 0;
+    }
+
+    int sign = 1;
+    if (c == '+' || c == '-')
+    {
+        if (c == '-')
+            sign = -1;
+    }
+    else
+        ungetc(c, f);
+
+    int value = 0;
+    int digits_read = 0;
+    while ((c = fgetc(f)) != EOF)
+    {
+        if (isdigit(c))
+        {
+            value = value * 10 + (c - '0');
+            digits_read++;
+        }
+        else
+        {
+            ungetc(c, f);
+            break;
+        }
+    }
+    
+    if (ferror(f))
+        return -1;
+    if (digits_read == 0)
+        return 0;
+    int *num_ptr = va_arg(ap, int *);
+    *num_ptr = value * sign;
+
+    return 1;
+}
+
+int scan_string(FILE *f, va_list ap)
+{
+    int c = fgetc(f);
+
+    if (c == EOF)
+    {
+        if (ferror(f))
+            return -1;
+        return 0;
+    }
+
+    ungetc(c, f);
+
+    char *s = va_arg(ap, char *);
+    int char_read = 0;
+    while((c = fgetc(f)) != EOF)
+    {
+        if (isspace(c))
+        {
+            ungetc(c, f);
+            break;
+        }
+        *s = (char)c;
+        s++;
+        char_read++;
+    }
+    if (ferror(f))
+        return -1;
+
+    if (!char_read)
+        return 0;
+    *s = '\0';
+    return 1;
+}
